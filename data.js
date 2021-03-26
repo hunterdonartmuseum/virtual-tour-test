49ec6b083c793cb4d3701eb7ee8366a1ebabec3f
hunterdonartmuseum <81400360+hunterdonartmuseum@users.noreply.github.com>
2021-03-25 23:07:45 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,79 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-photosphere",
+      "name": "photosphere",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.6598037986183574,
+        "pitch": 0,
+        "fov": 1.4837854729480213
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8904827059756641,
+          "pitch": 0.22774694756858338,
+          "rotation": 0,
+          "target": "1-jpeg-image-3a45e298a7ea-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5744045242423592,
+          "pitch": -0.05959377723268844,
+          "title": "test title",
+          "text": "test text<div><br></div><div>https://www.cloudpano.com/<br></div><div><br></div><div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-jpeg-image-3a45e298a7ea-1",
+      "name": "JPEG image-3A45E298A7EA-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
